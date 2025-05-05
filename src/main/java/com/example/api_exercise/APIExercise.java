0124e2a89cf208cfb99dd303f13d9d3d1335ae3f
Tyler Collingridge <tcollingridge@mines.edu>
2025-05-04 18:53:18 -0600

    move everything to main instead of in functions

--- src/main/java/com/example/api_exercise/APIExercise.java
+++ src/main/java/com/example/api_exercise/APIExercise.java
@@ -7,13 +7,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class APIExercise {
-    String urlString = "https://api.weather.gov";
-    URL url = null;
-    HttpURLConnection conn = null;
 
-//    establish connection in constructor
-    APIExercise() {
-//      create url
+    public static void main(String[] args) {
+        String urlString = "https://api.weather.gov";
+        URL url = null;
+        HttpURLConnection conn = null;
+//        create url
         try {
 //			URL is deprecated, use URI
 //			URL url = new URL(urlString);
@@ -31,30 +30,17 @@ public class APIExercise {
         } catch(Exception e) {
             System.out.println(e);
         }
-    }
 
-    //	for testing connection
-    int getResponseCode() {
+        // get response code
         int responseCode = -1;
         try {
             responseCode =  conn.getResponseCode();
         } catch(Exception e) {
             System.out.println(e);
         }
-        return responseCode;
-    }
-
-    void getForecast() {
-
-    }
-
-    public static void main(String[] args) {
-        APIExercise exercise = new APIExercise();
-//        exercise.getResponseCode(); // for testing
-        int rCode = exercise.getResponseCode();
-        if (rCode != 200) {
-            System.out.println("Error connecting to API, Response Code: " + rCode);
-        } else if (rCode == -1) {
+        if (responseCode != 200) {
+            System.out.println("Error connecting to API, Response Code: " + responseCode);
+        } else if (responseCode == -1) {
             System.out.println("Error getting response code");
         } else {
             System.out.println("Successfully connected to API...");
@@ -63,11 +49,37 @@ public class APIExercise {
             Scanner scanner = new Scanner(System.in);
             String userInput;
             do {
-                System.out.println("Enter a state (q to quit): ");
+                System.out.println("Enter a latitude and longitude (00.0000,00.0000), or press 'q' to quit: ");
                 userInput = scanner.nextLine();
-//                TODO: format user input to ensure that states are correctly formatted
-//                TODO: api call on given state
-                System.out.println(userInput);
+//                TODO: parse input to get lat and long as doubles
+                double lat;
+                double lon;
+                if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
+                    System.out.println("Invalid coordinates");
+                } else {
+//                  ensure that lat and lon are two digits and four decimal places (required by the api)
+//                  https://www.geeksforgeeks.org/how-to-set-precision-for-double-values-in-java/
+
+                    String sLat = String.format("%.4f", lat);
+                    String sLon = String.format("%.4f", lon);
+//                  retrieve metadata for location based on lat and lon
+                    try {
+                        String inline = "";
+//                        TODO: create url here and give it lat and lon
+                        //                TODO: api call on given coords
+                        Scanner scanner = new Scanner(url.openStream());
+                        while (scanner.hasNext()) {
+                            inline += scanner.nextLine();
+                        }
+                        scanner.close();
+                        System.out.println(inline);
+                    } catch (Exception e){
+                        System.out.println(e);
+                    }
+
+                }
+
+//                    System.out.println(userInput);
             } while ( !(userInput.toLowerCase(Locale.ROOT).equals("q")) );
             scanner.close();
         }
