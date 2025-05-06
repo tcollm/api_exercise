05d2cae9d29001ffa796abbd3ff04a3775a3de81
Tyler Collingridge <tcollingridge@mines.edu>
2025-05-05 21:16:30 -0600

    create pretty outputs for daily and hourly forecasts

--- src/main/java/com/example/api_exercise/APIExercise.java
+++ src/main/java/com/example/api_exercise/APIExercise.java
@@ -7,9 +7,15 @@ import java.net.URLConnection;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 import org.json.*;
+
 
 public class APIExercise {
 
@@ -139,12 +145,153 @@ public class APIExercise {
                                                 System.out.println(e);
                                             }
 
-                                            System.out.println(response1.toString());
+//                                            System.out.println(response1.toString());
+                                            JSONObject responseJson = new JSONObject(response1.toString());
+                                            JSONObject props1 = responseJson.getJSONObject("properties");
+//                                            extract "periods" as a json array
+                                            JSONArray periodsArray = props1.getJSONArray("periods");
 
+//                                            make the output pretty depending on if it's daily or hourly
+                                            if (userInput.toLowerCase(Locale.ROOT).equals("d")) { // get daily forecast
+//                                                for each period in periods
+//                                                  get name
+//                                                  temperature (with unit)
+//                                                  probabilityOfPrecipitation.value
+//                                                  windSpeed (with windDirection)
+//                                                  detailedForecast
+
+//                                                for each period in the periods array: get the name of the day, the temp,
+//                                                  the probability of precipitation, the windSpeed, and a detailed forecast
+                                                for (int i = 0; i < periodsArray.length(); i++) {
+                                                    JSONObject period = periodsArray.getJSONObject(i);
+
+//                                                    day of the week
+                                                    if (!period.isNull("name")) {
+                                                        System.out.println(period.getString("name"));
+                                                    } else {
+                                                        System.out.println("Day of the Week: N/A");
+                                                    }
+
+//                                                     temp (check for null values from api
+                                                    if ( !period.isNull("temperature") ) {
+                                                        if ( !period.isNull("temperatureUnit") ) {
+                                                            System.out.println("Temperature: " + period.getInt("temperature") + "° " + period.getString("temperatureUnit"));
+                                                        } else {
+                                                            System.out.println("Temperature: " + period.getInt("temperature") + "° ");
+                                                        }
+                                                    } else {
+                                                        System.out.println("Temperature: N/A");
+                                                    }
+
+//                                                     precipitation
+                                                    if (!period.getJSONObject("probabilityOfPrecipitation").isNull("value")) {
+                                                        System.out.println("Chance of precipitation: " + period.getJSONObject("probabilityOfPrecipitation").getInt("value") + "%");
+                                                    } else {
+                                                        System.out.println("Chance of precipitation: N/A");
+                                                    }
+//                                                     wind
+                                                    if (!period.isNull("wind")) {
+                                                        if (!period.isNull("windDirection")) {
+                                                            System.out.println("Wind: " + period.getString("windSpeed") + " " + period.getString("windDirection"));
+                                                        } else {
+                                                            System.out.println("Wind: " + period.getString("windSpeed"));
+                                                        }
+                                                    } else {
+                                                        System.out.println("Wind: N/A");
+                                                    }
+
+//                                                    detailed forecast
+                                                    System.out.println("Forecast:");
+                                                    if (!period.isNull("detailedForecast")) {
+                                                        System.out.println(period.getString("detailedForecast"));
+                                                    } else {
+                                                        System.out.println("N/A");
+                                                    }
+                                                    System.out.println(); // empty space for readability
+                                                }
+                                            } else if (userInput.toLowerCase(Locale.ROOT).equals("h")) { // get hourly
+//                                                for each period in periods
+//                                                  get display start time - end time
+
+//                                                  relativeHumidity.value
+//                                                  windSpeed (with windDirection)
+//                                                  shortForecast
+
+                                                for (int i = 0; i < periodsArray.length(); i++) {
+                                                    JSONObject period = periodsArray.getJSONObject(i);
+
+//                                                    time
+                                                    String startTime = "";
+                                                    String endTime = "";
+                                                    if (!period.isNull("startTime")) {
+//                                                        TODO: ensure that startTime always follows this format
+                                                        startTime = period.getString("startTime").substring(11, 16);;
+                                                    } else {
+                                                        startTime = "N/A";
+                                                    }
+                                                    if (!period.isNull("endTime")) {
+                                                        endTime = period.getString("endTime").substring(11, 16);;
+                                                    } else {
+                                                        endTime = "N/A";
+                                                    }
+
+                                                    System.out.println(startTime + " - " + endTime);
+
+//                                                     temp (check for null values from api
+                                                    if ( !period.isNull("temperature") ) {
+                                                        if ( !period.isNull("temperatureUnit") ) {
+                                                            System.out.println("Temperature: " + period.getInt("temperature") + "° " + period.getString("temperatureUnit"));
+                                                        } else {
+                                                            System.out.println("Temperature: " + period.getInt("temperature") + "° ");
+                                                        }
+                                                    } else {
+                                                        System.out.println("Temperature: N/A");
+                                                    }
+
+//                                                     precipitation
+                                                    if (!period.getJSONObject("probabilityOfPrecipitation").isNull("value")) {
+                                                        System.out.println("Chance of precipitation: " + period.getJSONObject("probabilityOfPrecipitation").getInt("value") + "%");
+                                                    } else {
+                                                        System.out.println("Chance of precipitation: N/A");
+                                                    }
+
+//                                                    dewpoint
+                                                    if (!period.getJSONObject("dewpoint").isNull("value")) {
+                                                        System.out.println("Dewpoint: " + period.getJSONObject("dewpoint").getInt("value") + "°");
+                                                    } else {
+                                                        System.out.println("Dewpoint: N/A");
+                                                    }
+
+//                                                     relative humidity
+                                                    if (!period.getJSONObject("relativeHumidity").isNull("value")) {
+                                                        System.out.println("Relative Humidity: " + period.getJSONObject("relativeHumidity").getInt("value") + "%");
+                                                    } else {
+                                                        System.out.println("Relative Humidity: N/A");
+                                                    }
+
+//                                                     wind
+                                                    if (!period.isNull("wind")) {
+                                                        if (!period.isNull("windDirection")) {
+                                                            System.out.println("Wind: " + period.getString("windSpeed") + " " + period.getString("windDirection"));
+                                                        } else {
+                                                            System.out.println("Wind: " + period.getString("windSpeed"));
+                                                        }
+                                                    } else {
+                                                        System.out.println("Wind: N/A");
+                                                    }
+
+//                                                    short forecast
+                                                    if (!period.isNull("shortForecast")) {
+                                                        System.out.println("Forecast: " + period.getString("shortForecast"));
+                                                    } else {
+                                                        System.out.println("N/A");
+                                                    }
+                                                    System.out.println(); // empty space for readability
+                                                }
+
+                                            }
                                             break;
                                         }
-
-
                                     }
                                 }
                             } catch (IOException e) {
